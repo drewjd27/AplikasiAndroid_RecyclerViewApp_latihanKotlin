b96d97eae10057354b78e9bfa40880743fa394e6
Andrew Jonatan Damanik <andrewdamanik23@gmail.com>
2022-08-16 20:55:28 +0700

    Added item_row_hero layout and Hero class.

---
+++ app/src/main/java/com/andrew/recylerviewapp/Hero.kt
@@ -0,0 +1,9 @@
+package com.andrew.recylerviewapp
+
+class Hero {
+    data class Hero (
+        var name: String = "",
+        var detail: String = "",
+        var photo: Int = 0
+            )
+}
